99c1a6409cb9bec316116d95a36ccf293bb85114
liuyoshio <liuyoshio2001@gmail.com>
2024-12-17 20:57:17 +0800

    Update featureData.ts

--- src/components/WhatIsWave/featureData.ts
+++ src/components/WhatIsWave/featureData.ts
@@ -8,7 +8,7 @@ export const features = [
                   Our platform eliminates the need for manual configuration, offering 
                   a streamlined experience that saves you time and effort.`,
     icon: Hammer,
-    imageUrl: 'https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&q=80',
+    imageUrl: 'https://plus.unsplash.com/premium_photo-1677094310919-d0361465d3be?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     title: 'Tool Upload and Sharing',
@@ -16,7 +16,7 @@ export const features = [
                   but also supports uploading your own tools to share with the community. Expand 
                   the ecosystem and make your tools available with just a few clicks.`,
     icon: CloudUpload,
-    imageUrl: 'https://images.unsplash.com/photo-1558494949-ef010cbdcc31?auto=format&fit=crop&q=80',
+    imageUrl: 'https://liblibai-tmp-image.liblib.cloud/sd-images/526c597435b8d93a3d10e7841fc55ecc36baa503218e068a00639fca2b35b678.png?Token=dbc2276635064414875973da815a569f',
   },
   {
     title: 'AI-Assisted Workflow',
@@ -24,6 +24,6 @@ export const features = [
                   and guidance on using MCP tools, optimizing workflows, and troubleshooting—all 
                   powered by AI for an enhanced experience.`,
     icon: SendHorizontal,
-    imageUrl: 'https://images.unsplash.com/photo-1552664730-d307ca884978?auto=format&fit=crop&q=80',
+    imageUrl: 'https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
 ];
